0c261075dfb3004bbe55128861c5fe290748c7ab
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-02-06 16:31:42 +0530

    import the About Us component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import AboutUs from "./Components/AboutUs";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
+      <AboutUs />
     </div>
   );
 };
